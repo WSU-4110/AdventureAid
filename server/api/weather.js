fe46309e1246748451d232606484cf4c8ac563fe
Aayush Patel <hd3998@wayne.edu>
2023-09-27 19:12:51 -0400

    integrated server with weather

--- server/api/weather.js
+++ server/api/weather.js
@@ -1,45 +1,44 @@
-require('dotenv').config(); // Load environment variables from the .env file
-const express = require('express'); // import express from 'express';
-const axios = require('axios'); // import axios from 'axios';
+const express = require('express'); // Import express
+const axios = require('axios'); // Import axios for making HTTP requests
+require('dotenv').config(); // Import dotenv for accessing environment variables
 
-const app = express(); // Create an instance of an Express app (server) 
+const router = express.Router(); // Create a router for your weather endpoints
+const API_KEY = process.env.OPEN_WEATHER_API_KEY; // Get the API key from the environment variables
 
-const API_KEY = process.env.OPEN_WEATHER_API_KEY;  // Assumes the key is stored in environment variables
-
-app.get('/weather/:city', async (req, res) => { // GET /weather/:city 
+router.get('/:city', async (req, res) => { // Define a route for GET /api/weather/:city 
     let url; // Declare a variable to hold the URL
-    const { city, cityId, lat, lon, zipCode } = req.params; // Extract the city name from the request parameters
-    if (city) { // If the city name is provided, use it to build the URL
+    const { city } = req.params; // Extract the city name from the request parameters
+    const { cityId, lat, lon, zipCode } = req.query; // Using query parameters for additional data
+
+    if (city) {
         url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`;
-    } else if (cityId) { // If the city ID is provided, use it to build the URL
+    } else if (cityId) {
         url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${API_KEY}&units=imperial`;
-    } else if (lat && lon) { // If the latitude and longitude are provided, use them to build the URL
+    } else if (lat && lon) {
         url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
-    } else if (zipCode) { // If the zip code is provided, use it to build the URL
+    } else if (zipCode) {
         url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${API_KEY}&units=imperial`;
-    } else { // If no location information is provided, send an error message back to the client
-        res.status(400).send({ message: 'No location information provided' });
+    } else {
+        return res.status(400).send({ message: 'No location information provided' });
     }
 
-    try { // Try to make the API call 
+    try {
         const response = await axios.get(url); // Wait for the response 
         const data = response.data; // Extract the data from the response 
-        res.send({ // Send the data back to the client
+        res.send({
             city: data.name, // Extract the city name
             temperature: data.main.temp, // Extract the temperature
             weather: data.weather[0].description // Extract the weather description
         });
-    } catch (error) { // Catch any errors
+    } catch (error) {
         if (error.response) { // If the error has a response property, it's a request error 
-            res.status(error.response.status).send({ message: 'Error retrieving weather data', error: error.response.data }); // Send the error response back to the client 
+            return res.status(error.response.status).send({ message: 'Error retrieving weather data', error: error.response.data });
         } else { // Otherwise, it's an error with the server/service 
-            res.status(500).send({ message: 'Unknown error', error: error.message }); // Send a generic 500 status with the error message 
+            return res.status(500).send({ message: 'Unknown error', error: error.message });
         }
     }
     
 });
 
 
-
-app.listen(3000, () => console.log('Server started on port 3000')); // Start the server on port 3000 and log a message to the console when it starts
-
+module.exports = router; // Export the router
