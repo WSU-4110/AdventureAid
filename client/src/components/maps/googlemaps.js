aef04b1ddcf27be5c4f5d1a96592cb2d17c11544
Hamzah Shakeel <77524241+shakeelh5802@users.noreply.github.com>
2023-10-23 11:16:32 -0400

    Update googlemaps.js
    reverted map size

--- client/src/components/maps/googlemaps.js
+++ client/src/components/maps/googlemaps.js
@@ -58,7 +58,7 @@ function MapComponent() {
         alignItems="center"
         opacity= "0.5"
     >
-        <Box id="map" sx={{ width: '95%', height: '400px' }} />
+        <Box id="map" sx={{ width: '75%', height: '400px' }} />
         <Box id="routeInfo" sx={{ marginTop: '1rem' }}>
             <span id="distance"></span>
             <span id="duration"></span>
