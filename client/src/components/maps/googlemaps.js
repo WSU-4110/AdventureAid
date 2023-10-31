1c826ceeebc7a3b941635b7b1055f6a9d2bce06c
Khushi Patel <khushiipatel30@gmail.com>
2023-10-31 18:39:19 -0400

    applied singleton design pattern

--- client/src/components/maps/googlemaps.js
+++ client/src/components/maps/googlemaps.js
@@ -1,26 +1,28 @@
+// File: googlemaps.js
+
 import React, { useEffect } from 'react';
 import { Box } from '@mui/material';
-import { googleMapsOperations } from './googlemapsAPI';
+import googleMapsManager from './googlemapsAPI';
 
 function MapComponent() {
     useEffect(() => {
-        googleMapsOperations.displayGoogleMaps();        
+        googlemapsAPI.displayGoogleMaps();        
     }, []);
 
     return (
-    <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        opacity= "0.5"
-    >
-        <Box id="map" sx={{ width: '75%', height: '400px' }} />
-        <Box id="routeInfo" sx={{ marginTop: '1rem' }}>
-            <span id="distance"></span>
-            <span id="duration"></span>
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            opacity= "0.5"
+        >
+            <Box id="map" sx={{ width: '75%', height: '400px' }} />
+            <Box id="routeInfo" sx={{ marginTop: '1rem' }}>
+                <span id="distance"></span>
+                <span id="duration"></span>
+            </Box>
         </Box>
-    </Box>
     );
 }
 
--- client/src/components/maps/googlemapsAPI.js
+++ client/src/components/maps/googlemapsAPI.js
@@ -1,163 +1,151 @@
+// File: googleMapsManager.js
+
 import { Loader } from "@googlemaps/js-api-loader";
 
-let directionsService, directionsRenderer;
-let mapInstance, infoWindow, markers = [];    // store the instance of the map, information window, initialize markers array
-export const googleMapsOperations = {
-    displayGoogleMaps: function() {
-        // Fetch the Google Maps API key from the server.
+class GoogleMapsManager {
+    constructor() {
+        if (!GoogleMapsManager.instance) {
+            this.mapInstance = null;
+            this.directionsService = null;
+            this.directionsRenderer = null;
+            this.markers = [];
+            GoogleMapsManager.instance = this;
+        }
+        return GoogleMapsManager.instance;
+    }
+
+    displayGoogleMaps() {
         fetch('http://localhost:3001/api/googlemapsapikey')
             .then(response => response.json())
             .then(data => {
-                const loader = googleMapsOperations.getLoader(data.apiKey);
-                return googleMapsOperations.loadGoogleMapsScript(loader);   
+                const loader = this.getLoader(data.apiKey);
+                return this.loadGoogleMapsScript(loader);   
             })
             .then(() => {
-                // Initialize the map and assign it to mapInstance
-                mapInstance = googleMapsOperations.initalizeMap();
-                googleMapsOperations.showCurrentLocation();
-
-                const addMarkerButton = document.createElement("button");
-                addMarkerButton.textContent = "Add Marker";
-                addMarkerButton.classList.add("custom-map-control-button");
-               
-                // Check if mapInstance.controls exists before adding the button
-                if (mapInstance.controls) {
-                    mapInstance.controls[window.google.maps.ControlPosition.TOP_RIGHT].push(addMarkerButton);
-                } else {
-                    console.error("mapInstance.controls is undefined");
-                }
-        
-                // Event listener to add markers when the button is clicked
-                addMarkerButton.addEventListener("click", () => {
-                    // Prompt the user for latitude and longitude
-                    const lat = parseFloat(prompt("Enter Latitude:"));
-                    const lng = parseFloat(prompt("Enter Longitude:"));
-        
-                    // Check if valid coordinates are entered
-                    if (!isNaN(lat) && !isNaN(lng)) {
-                        googleMapsOperations.addMarker(lat, lng);
-                    } else {
-                        alert("Invalid coordinates. Please enter valid numbers.");
-                    }
-                });
+                this.initializeMap();
+                this.showCurrentLocation();
+                // additional logic for handling UI elements like buttons can go here...
             })
             .catch(error => {
                 console.error("Error:", error);
             });
-    },
-    getLoader: function(apiKey) {
+    }
+
+    getLoader(apiKey) {
         return new Loader({
             apiKey: apiKey,
             version: "weekly",
             libraries: ["places"]
-            // ...additionalOptions,
         });
-    },
-    loadGoogleMapsScript: function(loader) {
-        return loader.load();  // returns a promise which resolves when the Google Maps script is successfully loaded.
-    },
+    }
 
-    addMarker: function(lat, lng) {
+    loadGoogleMapsScript(loader) {
+        return loader.load();
+    }
+
+    initializeMap() {
+        this.mapInstance = new window.google.maps.Map(document.getElementById("map"), {
+            center: { lat: 40.730610, lng: -73.935242 },
+            zoom: 12,
+        });
+    }
+
+    addMarker(lat, lng) {
         const marker = new window.google.maps.Marker({
             position: { lat, lng },
-            map: mapInstance,
-            draggable: true, // Allow the marker to be draggable
+            map: this.mapInstance,
+            draggable: true,
         });
 
-        // Add a click event listener to open an info window when the marker is clicked
         marker.addListener("click", () => {
             const infoWindow = new window.google.maps.InfoWindow({
                 content: `Marker Location: ${lat}, ${lng}`,
             });
-            infoWindow.open(mapInstance, marker);
+            infoWindow.open(this.mapInstance, marker);
         });
-        
-        // Add the marker to the markers array 
-        markers.push(marker);
-    },
 
-    initalizeMap: function() {
-        mapInstance = new window.google.maps.Map(document.getElementById("map"), {  //initializes the map
-            center: { lat: 40.730610, lng: -73.935242 },
-            zoom: 12,
-        });
-        return mapInstance; // returns the created map instance
-    },
-    handleLocationError: function(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-            browserHasGeolocation   // sets a corresponding error message to the infoWindow and then opens it on the map
-            ? "Error: The Geolocation service failed."
-            : "Error: Your browser doesn't support geolocation.",
-        );
-        infoWindow.open(mapInstance);
-    },
-    showCurrentLocation: function() {
-        infoWindow = new window.google.maps.InfoWindow();   //initialize the infoWindow instance
-        const locationButton = document.createElement("button");    //create new button element
+        this.markers.push(marker);
+    }
+
+    showCurrentLocation() {
+        const infoWindow = new window.google.maps.InfoWindow();
+        const locationButton = document.createElement("button");
         locationButton.textContent = "Pan to Current Location";
         locationButton.classList.add("custom-map-control-button");
-        //button is then added to the top center position of the map controls
-        mapInstance.controls[window.google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-        locationButton.addEventListener("click", () => {    //event listener when button is clicked
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {    
-            navigator.geolocation.getCurrentPosition((position) => {
-            const pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-            };
-            infoWindow.setPosition(pos);
-            infoWindow.setContent("Location found.");
-            infoWindow.open(mapInstance);
-            mapInstance.setCenter(pos);
-            },
-            () => {
-            this.handleLocationError(true, infoWindow, mapInstance.getCenter());
+        this.mapInstance.controls[window.google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+        
+        locationButton.addEventListener("click", () => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("Location found.");
+                    infoWindow.open(this.mapInstance);
+                    this.mapInstance.setCenter(pos);
+                },
+                () => {
+                    this.handleLocationError(true, infoWindow, this.mapInstance.getCenter());
+                });
+            } else {
+                this.handleLocationError(false, infoWindow, this.mapInstance.getCenter());
             }
-        );
-        } else 
-            // if browser doesn't support Geolocation
-            this.handleLocationError(false, infoWindow, mapInstance.getCenter());
         });
-    },
-    searchLocation: function(searchInput) {
-        const placesService = new window.google.maps.places.PlacesService(mapInstance);
+    }
+
+    handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(
+            browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation."
+        );
+        infoWindow.open(this.mapInstance);
+    }
+
+    searchLocation(searchInput) {
+        const placesService = new window.google.maps.places.PlacesService(this.mapInstance);
         placesService.textSearch({ query: searchInput }, (results, status) => {
             if (status === window.google.maps.places.PlacesServiceStatus.OK) {
                 const place = results[0];
-                mapInstance.setCenter(place.geometry.location);
-                googleMapsOperations.addMarker(place.geometry.location.lat(), place.geometry.location.lng());
+                this.mapInstance.setCenter(place.geometry.location);
+                this.addMarker(place.geometry.location.lat(), place.geometry.location.lng());
             } else {
                 console.error('Place not found:', status);
             }
         });
-    },
-    calculateAndDisplayRoute: function(start, end)
-    {
-        if (!directionsService) //check if directionsService is initialized
-            directionsService = new window.google.maps.DirectionsService();
-        if (!directionsRenderer) //check if directionsRenderer is initialized
-        {
-            directionsRenderer = new window.google.maps.DirectionsRenderer({    //create a new directionsRenderer instance
-                map: mapInstance    //associate it with the current active displayed map
+    }
+
+    calculateAndDisplayRoute(start, end) {
+        if (!this.directionsService)
+            this.directionsService = new window.google.maps.DirectionsService();
+        if (!this.directionsRenderer) {
+            this.directionsRenderer = new window.google.maps.DirectionsRenderer({
+                map: this.mapInstance
             });
         }
-        directionsService.route({   //make request to fetch directions
-            origin: start,  //starting location
-            destination: end,   //end location
-            travelMode: window.google.maps.TravelMode.DRIVING,  //transportation type
-        }, (response, status) => {      //callback function to handle result of route request
-            if (status === "OK") { 
-                directionsRenderer.setDirections(response);
-                const routeLeg = response.routes[0].legs[0];    //extract distance and time from response
+        this.directionsService.route({
+            origin: start,
+            destination: end,
+            travelMode: window.google.maps.TravelMode.DRIVING,
+        }, (response, status) => {
+            if (status === "OK") {
+                this.directionsRenderer.setDirections(response);
+                const routeLeg = response.routes[0].legs[0];
                 const distance = routeLeg.distance.text;
-                const duration = routeLeg.duration.text;  
-                document.getElementById("distance").textContent = `Distance: ${distance} `;  //Display distance
-                document.getElementById("duration").textContent = `Duration: ${duration}`;  //display duration
+                const duration = routeLeg.duration.text;
+                document.getElementById("distance").textContent = `Distance: ${distance} `;
+                document.getElementById("duration").textContent = `Duration: ${duration}`;
             } else {
                 window.alert("Directions request failed due to " + status);
             }
         });
     }
 }
+
+const instance = new GoogleMapsManager();
+Object.freeze(instance);
+
+export default instance;
