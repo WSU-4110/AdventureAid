0d61cc1b3eafed8d8acf8515be748fa4d51f1ed6
Aayush Patel <hd3998@wayne.edu>
2023-10-31 17:57:37 -0400

    refactored code using the Strategy Design Pattern

--- server/api/Weather/weather.js
+++ server/api/Weather/weather.js
@@ -1,50 +1,76 @@
-const express = require('express'); // Import express
-const axios = require('axios'); // Import axios for making HTTP requests
-require('dotenv').config(); // Import dotenv for accessing environment variables
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
 
-const router = express.Router(); // Create a router for your weather endpoints
-const API_KEY = process.env.OPEN_WEATHER_API_KEY; // Get the API key from the environment variables
+const router = express.Router();
+const API_KEY = process.env.OPEN_WEATHER_API_KEY;
 
-router.get('/:city', async (req, res) => { // Define a route for GET /api/weather/:city 
-    let url; // Declare a variable to hold the URL
-    const { city } = req.params; // Extract the city name from the request parameters
-    const { cityId, lat, lon, zipCode } = req.query; // Using query parameters for additional data
+class WeatherUrlBuilder {
+    buildUrl(params) {
+        throw new Error('This method should be overridden by subclass');
+    }
+}
 
-    // Build the URL based on the query parameters provided
+class CityUrlBuilder extends WeatherUrlBuilder {
+    buildUrl({ city }) {
+        return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`;
+    }
+}
+
+class CityIdUrlBuilder extends WeatherUrlBuilder {
+    buildUrl({ cityId }) {
+        return `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${API_KEY}&units=imperial`;
+    }
+}
+
+class LatLonUrlBuilder extends WeatherUrlBuilder {
+    buildUrl({ lat, lon }) {
+        return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
+    }
+}
+
+class ZipCodeUrlBuilder extends WeatherUrlBuilder {
+    buildUrl({ zipCode }) {
+        return `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${API_KEY}&units=imperial`;
+    }
+}
+
+router.get('/:city', async (req, res) => {
+    let urlBuilder;
+    const { city } = req.params;
+    const { cityId, lat, lon, zipCode } = req.query;
 
     if (city) {
-        url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`;
+        urlBuilder = new CityUrlBuilder();
     } else if (cityId) {
-        url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${API_KEY}&units=imperial`;
+        urlBuilder = new CityIdUrlBuilder();
     } else if (lat && lon) {
-        url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
+        urlBuilder = new LatLonUrlBuilder();
     } else if (zipCode) {
-        url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${API_KEY}&units=imperial`;
+        urlBuilder = new ZipCodeUrlBuilder();
     } else {
         return res.status(400).send({ message: 'No location information provided' });
     }
 
-    // Make the request to the OpenWeather API and return the response to the client 
+    const url = urlBuilder.buildUrl({ city, cityId, lat, lon, zipCode });
+
     try {
-        const response = await axios.get(url); // Wait for the response 
-        const data = response.data; // Extract the data from the response 
+        const response = await axios.get(url);
+        const data = response.data;
         res.send({
-            city: data.name, // Extract the city name
-            temperature: data.main.temp, // Extract the temperature
-            weather: data.weather[0].description, // Extract the weather description
-            humidity: data.main.humidity, // Extract the humidity
-            wind: data.wind.speed, // Extract the wind speed
+            city: data.name,
+            temperature: data.main.temp,
+            weather: data.weather[0].description,
+            humidity: data.main.humidity,
+            wind: data.wind.speed,
         });
-        // Handle errors
     } catch (error) {
-        if (error.response) { // If the error has a response property, it's a request error 
+        if (error.response) {
             return res.status(error.response.status).send({ message: 'Error retrieving weather data', error: error.response.data });
-        } else { // Otherwise, it's an error with the server/service 
+        } else {
             return res.status(500).send({ message: 'Unknown error', error: error.message });
         }
     }
-    
 });
 
-
-module.exports = router; // Export the router
+module.exports = router;
