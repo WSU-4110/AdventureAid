4a82de01a7bce64780a5313e29ebc731cfb2247c
Nisharg Patel <hk0307@wayne.edu>
2023-10-31 18:47:25 -0400

    implementing Singleton pattern

--- server/schemas/signupdata.js
+++ server/schemas/signupdata.js
@@ -1,24 +1,30 @@
-const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const signupSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        require:true,
-    },
-    lastname:{
-        type:String,
-        require:true,
-    },
-    email:{
-        type:String,
-        require:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        require:true
+class PasswordUtility {
+    static async hash(password) {
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10);  // added salt
+            return hashedPassword;
+        } catch (err) {
+            console.error(err);
+            throw new Error('Hashing failed');
+        }
     }
-});
 
-const User = new mongoose.model('User', signupSchema );
-module.exports = User;
+    static async compare(password, userPassword) {
+        try {
+            const isMatch = await bcrypt.compare(password, userPassword);
+            if (isMatch) {
+                console.log('Correct password');
+            } else {
+                console.log('Incorrect password');
+            }
+            return isMatch;
+        } catch (err) {
+            console.error(err);
+            throw new Error('Comparison failed');
+        }
+    }
+}
+
+module.exports = PasswordUtility;
