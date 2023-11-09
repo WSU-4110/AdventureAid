c6cc1a124cef9c610217d47c4a1bacb4185c0241
Iqra Shakeel <38725955+Shakeeli@users.noreply.github.com>
2023-11-09 15:37:06 -0500

    sign in and new here button design

--- client/src/screens/signup/index.js
+++ client/src/screens/signup/index.js
@@ -1,55 +1,42 @@
-// LoginScreen.js
 import React from "react";
 import { CssBaseline, Grid, Box, Typography, Button } from '@mui/material';
 import Helmet from 'react-helmet';
 
 import SignupForm from "../../components/signup";
-
 import { useNavigate } from 'react-router-dom';
 
-import './index.scss'; // Ensure your stylesheet is imported
+import './index.scss'; // Ensure your stylesheet is imported correctly
 
 function SignUpScreen({ onLogin }) {
-
-
   const navigate = useNavigate();
-   const handleHaveAnAccount = () => {
-     navigate('/login');
-    }
-
+  const handleHaveAnAccount = () => {
+    navigate('/login');
+  }
 
   return (
-      
-      // <Container maxWidth={false} className="hero-container">
-        <>
+    <>
       <CssBaseline />
       <Helmet>
-        <title>Sign up</title>
-        <meta name="description" content="" />
+        <title>Sign up - Adventure Aid</title>
+        <meta name="description" content="Join Adventure Aid and start planning your next journey!" />
       </Helmet>
       
-      <Grid
-        container
-        direction={"column"}
-        className="hero-container" 
-      >
+      <Grid container direction={"column"} className="hero-container">
         <Grid item>
           <Box className="logo-col">
-            <Typography variant="subtitle1" className="logo-text">
-              Already have an account?
-            </Typography>
-            <Button variant="h4" className="logo-text" onClick={handleHaveAnAccount}>
-              <Typography variant="h4" style={{ textDecoration: "none" }}> Sign in </Typography>
-            </Button>
+            <Typography variant="h5" component="h2" className="account-query" mb="2rem"> Already Have an Account? </Typography>
+            
+            
+            <Button variant="contained" onClick={handleHaveAnAccount} className="sign-in-button"> Sign in </Button>
           </Box>
         </Grid>
         <Grid item xs="auto" className="login-component"> 
-        <SignupForm />
+          <SignupForm />
         </Grid>
-      </Grid></>
-   
-      
+      </Grid>
+    </>
   );
 }
 
 export default SignUpScreen;
+
