2e67431f1c87971cec676e1c3fd07ec06e916913
Dan Pop <hc7822@wayne.edu>
2023-11-15 11:09:00 -0500

    created and routed vacation class and its operations

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -8,3 +8,44 @@ app.post('/signup', (req, res) => {
   res.send('Received your request!');
 });
 */
+
+class Vacation {
+  constructor() {
+    this.destinations = [];
+    this.numOfDestinations = 0;
+  }
+
+  pushDestination(destination) {
+    if (!destination || typeof destination !== 'string') {
+      throw new Error('Invalid destination');
+    }
+    this.destinations.push(destination);  //enqueue
+    this.numOfDestinations += 1
+  }
+
+  removeDestination(destination) {
+    if (!destination || typeof destination !== 'string') {
+      throw new Error('Invalid destination');
+    }
+    if (this.numOfDestinations == 0) {
+      throw new Error('Destinations array is already empty');
+    }
+    this.destinations = this.destinations.filter(item => item !== destination);
+    this.numOfDestinations -= 1
+  }
+
+  removeAllDestinations() {
+    this.destinations = [];
+    this.numOfDestinations = 0;
+  }
+
+  getAllDestinations() {
+    return this.destinations;
+  }
+
+  getNumOfDestinations() {
+    return this.numOfDestinations;
+  }
+}
+
+module.exports = Vacation;
