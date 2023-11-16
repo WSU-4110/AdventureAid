10a2bc4177a023c84cc28609699333f2dda98f63
Nisharg Patel <hk0307@wayne.edu>
2023-11-15 22:00:36 -0500

    implemented signup and login in client side

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,23 +1,47 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; 
 import { Container, Typography, TextField, Checkbox, FormControlLabel, Button, Link, CssBaseline, Paper} from '@mui/material';
 import Helmet from 'react-helmet';
+import axios from 'axios';
 
 import './index.scss';
 
-function LoginForm({ onLogin, navigate }) {
+function LoginForm({ onLogin }) {
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('login');
-    if (typeof onLogin === 'function') { // Now you can directly use onLogin without props.
-      onLogin(); // Call the onLogin prop function to simulate successful login
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  setErrorMsg(''); 
+  try {
+    const response = await axios.post('http://localhost:3001/api/loginuser', {
+      email,
+      password,
+    });
+    
+    const { Token } = response.data;
+console.log('token recall',response);//for test
+
+    if (Token) { //
+      localStorage.setItem('authToken', Token);
+      if (onLogin) onLogin();
       navigate('/'); // Navigate to the home page
+    } else {
+      setErrorMsg('Login failed: No token received.');
     }
-  };
-
+  } catch (error) {
+    if (error.response) {
+      setErrorMsg(`Login failed: ${error.response.data.message}`);
+    } else if (error.request) {
+      setErrorMsg('Login failed: No response from the server.');
+    } else {
+      setErrorMsg('Login failed: An error occurred.');
+    }
+  }
+};
 
   return (
     <Container component="main" maxWidth={false}>
@@ -29,6 +53,7 @@ function LoginForm({ onLogin, navigate }) {
 
       <Paper elevation={3} className="login-paper">
         <Typography variant="h5">Sign in</Typography>
+        {errorMsg && <div className="error-message">{errorMsg}</div>}
         <form className="login-form" onSubmit={handleSubmit}>
           <TextField
             variant="outlined"
@@ -68,7 +93,7 @@ function LoginForm({ onLogin, navigate }) {
             }
             label={<Typography className="checkbox-label">Remember me</Typography>}
           />
-          <Button variant="contained" className="custom-button" onClick={handleSubmit}>
+          <Button type="submit" variant="contained" className="custom-button" onClick={handleSubmit}>
           Sign In
         </Button>
           <div className="forgot-password">
