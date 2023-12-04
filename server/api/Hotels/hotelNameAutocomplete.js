85caed92aeb64a16f2743dacf5d72725f0470fb7
Khushi Patel <khushiipatel30@gmail.com>
2023-12-04 15:50:51 -0500

    hotel autocomplete endpoint

--- server/api/Hotels/hotelNameAutocomplete.js
+++ server/api/Hotels/hotelNameAutocomplete.js
@@ -28,4 +28,79 @@ async function getAmadeusToken() {
     }
 }
 
+
+// Function to construct the URL for hotel search
+function constructHotelSearchURL(endpoint, params) {
+    let url = `https://test.api.amadeus.com/v1/reference-data/locations/${endpoint}?`;
+
+    for (let key in params) {
+        if (params.hasOwnProperty(key) && params[key]) {
+            url += `${key}=${encodeURIComponent(params[key])}&`;
+        }
+    }
+
+    return url.slice(0, -1);  // Remove the trailing '&'
+}
+
+// Hotel name autocomplete endpoint
+router.get('/hotels/autocomplete', async (req, res) => {
+    const { keyword, subType } = req.query;
+
+    if (!keyword) {
+        return res.status(400).send({ message: 'Required parameter: keyword.' });
+    }
+
+    if(!subType) {
+        return res.status(400).send({ message: 'Required parameter: subType.' });
+    }
+
+    let subTypes = Array.isArray(subType) ? subType : [subType]; // Convert to array if not already
+
+    // Validate subTypes array to only contain allowed values
+    const allowedSubTypes = ['HOTEL_LEISURE', 'HOTEL_GDS'];
+    subTypes = subTypes.filter(type => allowedSubTypes.includes(type));
+
+    if (subTypes.length === 0) {
+        return res.status(400).send({ message: 'Invalid subType parameter. Must be "HOTEL_LEISURE" or "HOTEL_GDS".' });
+    }
+
+    let token;
+    try {
+        token = await getAmadeusToken();
+    } catch (error) {
+        return res.status(500).send({ message: 'Failed to authenticate with Amadeus', error: error.message });
+    }
+
+    const queryParams = {
+        keyword,
+        subType: subTypes.join(','), // Convert array to comma-separated string
+    };
+
+    try {
+        const hotelResponse = await axios.get(constructHotelSearchURL('hotel', queryParams), {  // 'autocomplete' replaced with 'hotel'
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        res.send(hotelResponse.data);
+    } catch (error) {
+        handleError(res, error);
+    }
+});
+
+// Central error handling function
+function handleError(res, error) {
+    if (error.response) {
+        return res.status(error.response.status).send({ message: 'Error retrieving hotel information', error: error.response.data });
+    } else {
+        return res.status(500).send({ message: 'Unknown error', error: error.message });
+    }
+}
+
+
 module.exports = router;
+
+
+// testing links:
+// http://localhost:3001/api/hotelNameAutocomplete/hotels/autocomplete?keyword=PARI&subType=HOTEL_LEISURE
+// http://localhost:3001/api/hotelNameAutocomplete/hotels/autocomplete?keyword=PARI&subType=HOTEL_GDS
