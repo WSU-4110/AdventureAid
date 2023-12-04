04e7c27ea932813dc993ebd7014010ba54271ff9
shakeelh5082 <shakeelh5802@gmail.com>
2023-12-04 17:09:15 -0500

    pulled main

--- server/server.js
+++ server/server.js
@@ -12,13 +12,14 @@ const flightSearchRoutes = require('./api/Flight/flightSearch.js');
 const hotelListRoutes = require('./api/Hotels/hotelList.js');
 const flightDelayPrediction = require('./api/Flight/flightDelayPrediction.js');
 require("dotenv").config();
-const Vacation = require("./middleware/index.js");
+const { Destination, Vacation } = require("./middleware/vacation.js");
 //const User = require('./schemas/signupdata');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userRoute = require('./routes/signupRoute.js');
-
+const hotelRating = require('./api/Hotels/hotelRating.js');
 const db = require('./api/mongoDB');
+const categoryRatedAreas = require('./api/Location/categoryRatedAreas.js');
 const { required } = require('joi');
 
 // Middleware
@@ -34,7 +35,9 @@ app.use('/api/flightStatus', flightStatusRoutes); // Enable the flight status ro
 app.use('/api/flightSearch', flightSearchRoutes); // Enable the flight search routes for the /api/flightSearch endpoint
 app.use('/api/hotelList', hotelListRoutes); // Enable the hotel list routes for the /api/hotelList endpoint
 app.use('/api',userRoute);
+app.use('/api/hotelRating', hotelRating); // Enable the hotel rating routes for the /api/hotelRating endpoint
 app.use('/api/flightDelayPrediction', flightDelayPrediction); // Enable the flight delay prediction routes for the /api/flightDelayPrediction endpoint
+app.use('/api/categoryRatedAreas', categoryRatedAreas); // Enable the category rated areas routes for the /api/categoryRatedAreas endpoint
 // Root Endpoint
 app.get('/', (req, res) => {
   res.send('Hello, Travel Planner!');
@@ -50,9 +53,21 @@ app.get('/api/googlemapsapikey', (req, res) => {
 
 const vacationInstance = new Vacation();
 
+app.post('/start-planning-vacation', (req,res) => {
+  try {
+    const { locality, startDate, endDate } = req.body;
+    vacationInstance.planVacation(locality, startDate, endDate);
+    console.log(vacationInstance);
+    res.status(200).send('Vacation created successfully');
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
 app.post('/push-destination', (req, res) => {
   try {
-      vacationInstance.pushDestination(req.body.destination);
+      let destinationInstance = new Destination(req.body.destination)
+      vacationInstance.pushDestination(destinationInstance);
       console.log(vacationInstance.getAllDestinations());
       res.status(200).send('Destination added successfully');
   } catch (error) {
@@ -75,6 +90,42 @@ app.post('/remove-all-destinations', (req, res) => {
     vacationInstance.removeAllDestinations();
     console.log(vacationInstance.getAllDestinations());
     res.status(200).send("All destinations deleted successfully");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+})
+
+app.post('/get-vacation-name', (req,res) => {
+  try {
+    const vacationName = vacationInstance.name;
+    res.status(200).send({vacationName});
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+})
+
+app.post('/get-vacation-locality', (req,res) => {
+  try {
+    const vacationLocality = vacationInstance.locality;
+    res.status(200).send({vacationLocality});
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+})
+
+app.post('/get-vacation-startdate', (req,res) => {
+  try {
+    const vacationStartDate = vacationInstance.startDate;
+    res.status(200).send({vacationStartDate});
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+})
+
+app.post('/get-vacation-enddate', (req,res) => {
+  try {
+    const vacationEndDate = vacationInstance.endDate;
+    res.status(200).send({vacationEndDate});
   } catch (error) {
     res.status(400).send(error.message);
   }
