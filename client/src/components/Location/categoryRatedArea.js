9d029a6e00d1fffd723310ae595a7c9b2f8486a9
Aayush Patel <hd3998@wayne.edu>
2023-12-07 20:08:41 -0500

    made the API look good on the website (frontend)

--- client/src/components/Location/categoryRatedArea.js
+++ client/src/components/Location/categoryRatedArea.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Container, TextField, Button, Typography, Paper, Grid } from '@mui/material';
-import './categoryRatedAreas.scss'; // Import the SCSS file
+import './categoryRatedAreas.scss';
 
 function CategoryRatedAreas() {
     const [latitude, setLatitude] = useState('');
@@ -11,7 +11,6 @@ function CategoryRatedAreas() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         try {
             const response = await axios.get(`http://localhost:3001/api/categoryRatedAreas/category-rated-areas`, {
                 params: { latitude, longitude }
@@ -24,38 +23,44 @@ function CategoryRatedAreas() {
         }
     };
 
+    const renderResult = () => {
+        if (!result || !result.data) return null;
+        return result.data.map((area, index) => (
+            <div key={index}>
+                <Typography variant="h6" gutterBottom>Area {index + 1}</Typography>
+                <Typography>Latitude: {area.geoCode.latitude}</Typography>
+                <Typography>Longitude: {area.geoCode.longitude}</Typography>
+                <Typography>Radius: {area.radius} meters</Typography>
+                <Typography variant="subtitle1">Category Scores:</Typography>
+                <Typography>Sight (Overall): {area.categoryScores.sight.overall}</Typography>
+                <Typography>Historical: {area.categoryScores.sight.historical}</Typography>
+                <Typography>Beach and Park: {area.categoryScores.sight.beachAndPark}</Typography>
+            </div>
+        ));
+    };
+
     return (
         <Container maxWidth="sm" className="category-rated-areas-container">
             <Paper elevation={6} style={{ padding: '20px', marginTop: '20px' }}>
                 <form onSubmit={handleSubmit}>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <label htmlFor="latitude" style={{ color: 'red', marginRight: '5px' }}>
-                                    Latitude *
-                                </label>
-                                <TextField
-                                    fullWidth
-                                    label="Latitude"
-                                    value={latitude}
-                                    onChange={(e) => setLatitude(e.target.value)}
-                                    required
-                                />
-                            </div>
+                            <TextField
+                                fullWidth
+                                label="Latitude"
+                                value={latitude}
+                                onChange={(e) => setLatitude(e.target.value)}
+                                required
+                            />
                         </Grid>
                         <Grid item xs={12}>
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <label htmlFor="longitude" style={{ color: 'red', marginRight: '5px' }}>
-                                    Longitude *
-                                </label>
-                                <TextField
-                                    fullWidth
-                                    label="Longitude"
-                                    value={longitude}
-                                    onChange={(e) => setLongitude(e.target.value)}
-                                    required
-                                />
-                            </div>
+                            <TextField
+                                fullWidth
+                                label="Longitude"
+                                value={longitude}
+                                onChange={(e) => setLongitude(e.target.value)}
+                                required
+                            />
                         </Grid>
                     </Grid>
                     <Button type="submit" variant="contained" color="primary" style={{ marginTop: '20px' }}>
@@ -63,11 +68,9 @@ function CategoryRatedAreas() {
                     </Button>
                 </form>
 
-                {result && (
-                    <Typography style={{ marginTop: '20px' }}>
-                        <pre>{JSON.stringify(result, null, 2)}</pre>
-                    </Typography>
-                )}
+                <div style={{ marginTop: '20px' }}>
+                    {renderResult()}
+                </div>
                 {error && <Typography color="error" style={{ marginTop: '20px' }}>Error: {error}</Typography>}
             </Paper>
         </Container>
